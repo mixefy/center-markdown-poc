3aa238619745e8d1e01239d366c7b95a1ab3a44d
Viktors Garkavijs <v@mixefy.jp>
2022-12-22 18:41:43 +0900

    The demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MarkdownModule } from "ngx-markdown";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MarkdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
